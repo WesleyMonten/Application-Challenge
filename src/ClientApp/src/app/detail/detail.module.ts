81d4c694cf4d079a1d56d4ae35a443ac20ec02de
Wesley Monten <wesleymonten123@gmail.com>
2019-12-10 20:15:45 +0100

    profile applicant

--- src/ClientApp/src/app/detail/detail.module.ts
+++ src/ClientApp/src/app/detail/detail.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StudentDetailComponent } from './student-detail/student-detail.component';
 import { CompanyDetailComponent } from './company-detail/company-detail.component';
 import { AssignmentDetailComponent } from './assignment-detail/assignment-detail.component';
 import { ReviewDetailComponent } from './review-detail/review-detail.component';
 import { ApplicantDetailComponent } from './applicant-detail/applicant-detail.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [StudentDetailComponent, CompanyDetailComponent, AssignmentDetailComponent, ReviewDetailComponent, ApplicantDetailComponent],
+  declarations: [CompanyDetailComponent, AssignmentDetailComponent, ReviewDetailComponent, ApplicantDetailComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class DetailModule { }
