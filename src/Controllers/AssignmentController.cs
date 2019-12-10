f3136b57f1f3acbb5d284f7fa2fd0bcdcaed910e
culacant <r0463164@student.thomasmore.be>
2019-12-10 15:14:00 +0100

    draf/open/we endpoints for assignments

--- src/Controllers/AssignmentController.cs
+++ src/Controllers/AssignmentController.cs
@@ -20,10 +20,32 @@ namespace ApplicationChallenge.Controllers
             {
                 return Assignments.Find(tag => true).ToList();
             }
+            [HttpGet("open")]
+            public IEnumerable<Assignment> GetOpen()
+            {
+                return Assignments.Find(assignment =>  assignment.Stage == AssignmentStage.Open).ToList();
+            }
+
             [HttpGet("company/{id}")]
             public IEnumerable<Assignment> GetByCompany(string id)
             {
                 return Assignments.Find(assignment => assignment.CompanyId == id).ToList();
+            }
+            [HttpGet("company/{id}/open")]
+            public IEnumerable<Assignment> GetOpenByCompany(string id)
+            {
+                return Assignments.Find(assignment => assignment.CompanyId == id && assignment.Stage == AssignmentStage.Open).ToList();
+            }
+            [HttpGet("company/{id}/closed")]
+            public IEnumerable<Assignment> GetClosedByCompany(string id)
+            {
+                return Assignments.Find(assignment => assignment.CompanyId == id && assignment.Stage == AssignmentStage.Closed).ToList();
+            }
+// alleen voor company/creator zelf
+            [HttpGet("company/{id}/draft")]
+            public IEnumerable<Assignment> GetDraftByCompany(string id)
+            {
+                return Assignments.Find(assignment => assignment.CompanyId == id && assignment.Stage == AssignmentStage.Draft).ToList();
             }
             [HttpGet("{id}")]
             public Assignment GetById(string id)
