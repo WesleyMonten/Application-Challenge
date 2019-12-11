f62e401cb3df2bf14e62e31d01f020dd68d800ed
HoLLy <dario.db@hotmail.com>
2019-12-12 00:26:07 +0100

    Add name to company model

--- src/Models/Company.cs
+++ src/Models/Company.cs
@@ -9,6 +9,10 @@ namespace ApplicationChallenge.Models
     public class Company
     {
         /// <summary>
+        /// The name of the <see cref="Company"/>
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
         /// Contact email of the <see cref="Company"/>, only to be used when <see cref="Assignment.Stage"/> is
         /// <see cref="AssignmentStage.Finished"/>
         /// </summary>
