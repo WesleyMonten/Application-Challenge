5a711054fec25a49d43b306c58398ae86915bb8a
HoLLy <dario.db@hotmail.com>
2019-12-13 00:21:29 +0100

    Add HttpInterceptor to automatically add token to requests

--- src/ClientApp/src/app/app.module.ts
+++ src/ClientApp/src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from './shared/shared.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { FeedModule } from './feed/feed.module';
-import { DetailModule } from './detail/detail.module';
-import { EditModule } from './edit/edit.module';
-import { DeleteModule } from './delete/delete.module';
-import { AccountService } from './services/account.service';
-import { AssignmentService } from './services/assignment.service';
-import { ApplicationService } from './services/application.service';
-import { CompanyService } from './services/company.service';
-import { SkillService } from './services/skill.service';
-import { ReviewService } from './services/review.service';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {AppComponent} from './app.component';
+import {NavMenuComponent} from './nav-menu/nav-menu.component';
+import {AppRoutingModule} from './app-routing.module';
+import {SharedModule} from './shared/shared.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {FeedModule} from './feed/feed.module';
+import {DetailModule} from './detail/detail.module';
+import {EditModule} from './edit/edit.module';
+import {DeleteModule} from './delete/delete.module';
+import {AccountService} from './services/account.service';
+import {AssignmentService} from './services/assignment.service';
+import {ApplicationService} from './services/application.service';
+import {CompanyService} from './services/company.service';
+import {SkillService} from './services/skill.service';
+import {ReviewService} from './services/review.service';
+import {SecurityInterceptor} from "./security/security.interceptor";
 
 @NgModule({
   declarations: [
@@ -45,7 +46,12 @@ import { ReviewService } from './services/review.service';
     ApplicationService,
     CompanyService,
     SkillService,
-    ReviewService
+    ReviewService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: SecurityInterceptor,
+      multi: true
+    },
   ],
   bootstrap: [AppComponent]
 })
