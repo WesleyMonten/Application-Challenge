cb4def3127f17f343e942611e1a8ee931f5fc412
HoLLy <dario.db@hotmail.com>
2019-12-13 14:48:55 +0100

    Add commendation controller

--- src/Models/Database/CompanyCommendation.cs
+++ src/Models/Database/CompanyCommendation.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel;
+
 namespace ApplicationChallenge.Models.Database
 {
     public enum CompanyCommendation
     {
+        [Description("Pleasant Atmosphere")]
         PleasantAtmosphere,
+        [Description("Stress-free Environment")]
         StressFreeEnvironment,
+        [Description("Good Accommodation")]
         GoodAccommodation,
     }
 }
