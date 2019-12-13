979b7bdde79915dab720c43153786c061e151b60
Wesley Monten <wesleymonten123@gmail.com>
2019-12-13 21:33:02 +0100

    layout assignment detail

--- src/ClientApp/src/app/app-routing.module.ts
+++ src/ClientApp/src/app/app-routing.module.ts
@@ -8,17 +8,15 @@ import { AccountDetailComponent } from './detail/account-detail/account-detail.c
 import { AccountEditComponent } from './edit/account-edit/account-edit.component';
 import { AssignmentDetailComponent } from './detail/assignment-detail/assignment-detail.component';
 import { AuthGuard } from './security/auth.guard';
-import { BoardComponent } from './detail/board/board.component';
 import { AssignmentBoardComponent } from './detail/assignment-board/assignment-board.component';
 
 const appRoutes: Routes = [
   { path: '', component: FeedComponent, pathMatch: 'full', canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
-  { path: 'board/:id', component: AssignmentBoardComponent, canActivate: [AuthGuard] },
   { path: 'register', component: RegisterComponent },
   { path: 'account/:id', component: AccountDetailComponent, canActivate: [AuthGuard] },
   { path: 'edit-account/:id', component: AccountEditComponent, canActivate: [AuthGuard] },
-  { path: 'assignments', component: AssignmentDetailComponent, canActivate: [AuthGuard] },
+  { path: 'assignments/:id', component: AssignmentDetailComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '' }
 ];
 
