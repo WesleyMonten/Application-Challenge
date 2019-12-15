20b2372da4533bc24be735a15edbd6d821c344ab
culacant <r0463164@student.thomasmore.be>
2019-12-15 20:16:37 +0100

    skill controllers

--- src/ClientApp/src/app/services/skill.service.ts
+++ src/ClientApp/src/app/services/skill.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Skill } from '../models/skill.model';
 import { of } from 'rxjs';
+import {Assignment} from "../models/assignment.model";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable()
 export class SkillService {
-
+// name en color met hoofdletters in DB
   skills: Array<Skill> = [
     { skillId: "1", name: ".NET", color: "#5C2D91" },
     { skillId: "2", name: "Angular", color: "#C3002F" },
@@ -13,9 +15,9 @@ export class SkillService {
     { skillId: "5", name: "Internship", color: "#0074C1" },
   ]
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get() {
-    return of(this.skills);
+    return this.http.get<Skill[]>("/skill/");
   }
 }
--- src/Models/Database/Skill.cs
+++ src/Models/Database/Skill.cs
@@ -1,10 +1,17 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace ApplicationChallenge.Models.Database
 {
     /// <summary>
     /// A skill that can be shown by an <see cref="Applicant"/>
     /// </summary>
-    public class Skill
+    public class Skill: IDatabaseModel
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        
         public string Name { get; set; }
         
         /// <summary>
