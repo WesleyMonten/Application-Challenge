1090a2e5dc0a75a1b192c7aa95331bd361388aa9
Wesley Monten <wesleymonten123@gmail.com>
2019-12-15 14:32:47 +0100

    ongebruikte package verwijderd

--- src/ClientApp/src/app/feed/feed.module.ts
+++ src/ClientApp/src/app/feed/feed.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedComponent } from './feed/feed.component';
 import { SharedModule } from '../shared/shared.module';
-import { ClipboardModule } from 'ngx-clipboard';
 
 @NgModule({
   declarations: [FeedComponent],
   imports: [
     CommonModule,
     SharedModule,
-    ClipboardModule
   ]
 })
 export class FeedModule { }
