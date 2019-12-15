8eee512ce29a4800d4e8d76cb92f0e399aed07e7
Wesley Monten <wesleymonten123@gmail.com>
2019-12-15 14:31:02 +0100

    tooltips assignment board

--- src/ClientApp/src/app/feed/feed.module.ts
+++ src/ClientApp/src/app/feed/feed.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedComponent } from './feed/feed.component';
 import { SharedModule } from '../shared/shared.module';
+import { ClipboardModule } from 'ngx-clipboard';
 
 @NgModule({
   declarations: [FeedComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ClipboardModule
   ]
 })
 export class FeedModule { }
