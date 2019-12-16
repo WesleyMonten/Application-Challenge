fbaff2485fa7fec86a6a9d193dadc09e6d41e9d4
Wesley Monten <wesleymonten123@gmail.com>
2019-12-16 07:22:09 +0100

    button feed

--- src/ClientApp/src/app/feed/feed/feed.component.ts
+++ src/ClientApp/src/app/feed/feed/feed.component.ts
@@ -4,9 +4,11 @@ import { Assignment } from 'src/app/models/assignment.model';
 import { SearchService } from 'src/app/services/search.service';
 import { ApplicationService } from 'src/app/services/application.service';
 import { Application } from 'src/app/models/application.model';
-import {UserInfoService} from "../../services/user-info.service";
-import {UserInfo} from "../../models/user-info";
-import {AccountService} from "../../services/account.service";
+import { UserInfoService } from "../../services/user-info.service";
+import { UserInfo } from "../../models/user-info";
+import { AccountService } from "../../services/account.service";
+import { Account } from 'src/app/models/account.model';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-feed',
@@ -14,6 +16,7 @@ import {AccountService} from "../../services/account.service";
   styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent implements OnInit {
+  account: Account;
   assignments: Assignment[];
   companies: UserInfo[] = [];
   liked: boolean = false;
@@ -23,7 +26,7 @@ export class FeedComponent implements OnInit {
   model: Application = new Application("", "", "", false);
 
   constructor(private _assignmentService: AssignmentService,
-    private _searchService: SearchService, private _applicationService: ApplicationService, private _userInfoService: UserInfoService, private _accountService: AccountService) {
+    private _searchService: SearchService, private _applicationService: ApplicationService, private _userInfoService: UserInfoService, private _accountService: AccountService, private _snackBar: MatSnackBar) {
     this._searchService.currentSearch.subscribe(result => {
       this.search = result;
       this.getOpenAssignments();
@@ -32,22 +35,16 @@ export class FeedComponent implements OnInit {
 
   ngOnInit() {
     this.getOpenAssignments();
+    this.getAccount();
   }
 
-
-  toggleLike() {
-    this.liked = this.liked != true;
+  getAccount() {
+    this._accountService.get("me").subscribe(res => {
+      this.account = res;
+    })
   }
 
-  toggleLike2() {
-    this.liked2 = this.liked2 != true;
-  }
-
-  toggleExpand() {
-    this.expanded = this.expanded != true
-  }
-
-  getAccountsAndCompaniesOfAssignments(assignments: Assignment[]) {
+  getCompaniesOfAssignments(assignments: Assignment[]) {
     assignments.forEach(a => {
       this.getCompanyOfAssignment(a.companyId);
     });
@@ -58,29 +55,36 @@ export class FeedComponent implements OnInit {
       this.companies.push(res);
     });
   }
-  getOpenAssignments(){
-    if(this.search == null || this.search == ""){
+
+  getOpenAssignments() {
+    if (this.search == null || this.search == "") {
       this._assignmentService.getAllOpenAssignments().subscribe(res => {
         this.assignments = res;
-        this.getAccountsAndCompaniesOfAssignments(res);
+        this.getCompaniesOfAssignments(res);
       })
     }
-    else{
+    else {
       this._assignmentService.getAllOpenAssignmentsByTitle(this.search).subscribe(res => {
         this.assignments = res;
-        this.getAccountsAndCompaniesOfAssignments(res);
+        this.getCompaniesOfAssignments(res);
       })
     }
   }
 
-  apply(assignment: string){
+  apply(assignment: string, event) {
+    console.log(event);
     this._accountService.get('me').subscribe(user => {
       this.model.assignmentId = assignment;
       this.model.accepted = false;
       this.model.applicantId = user.id;
       console.log(this.model);
       this._applicationService.addApplication(this.model).subscribe(result => {
+        event.target.hidden = true;
         console.log(result);
+        this._snackBar.open("Applied for the assignment!", "", {
+          duration: 2000,
+          panelClass: ['snackbar']
+        });
       })
     })
   }
