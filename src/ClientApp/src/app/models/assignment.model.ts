2cfc5c89867a3f234ff8a0377a26b2e2c375cf3a
HoLLy <dario.db@hotmail.com>
2019-12-16 03:58:13 +0100

    Fix assignment id

--- src/ClientApp/src/app/models/assignment.model.ts
+++ src/ClientApp/src/app/models/assignment.model.ts
@@ -2,5 +2,5 @@ import { AssignmentTopic } from "./assignment-topic.model";
 import { AssignmentStage } from "./assignment-stage.model";
 
 export class Assignment {
-    constructor(public assignmentId: string, public title: string, public description: string, public location: string, public startTime: Date, public endTime: Date, public isInternship: boolean, public compensation: number, public stage: AssignmentStage, public topics: AssignmentTopic[], public applicationId: string, public companyId: string) { }
+    constructor(public id: string, public title: string, public description: string, public location: string, public startTime: Date, public endTime: Date, public isInternship: boolean, public compensation: number, public stage: AssignmentStage, public topics: AssignmentTopic[], public applicationId: string, public companyId: string) { }
 }
